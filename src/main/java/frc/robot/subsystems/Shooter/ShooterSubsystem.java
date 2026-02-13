920c1f4dca1518aeb6e51f4c8649b16af88267d3
JChiu8 <119461033+JChiu8@users.noreply.github.com>
2026-02-13 10:05:40 -0800

    Basic Climber Code

--- src/main/java/frc/robot/subsystems/Shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterSubsystem.java
@@ -8,7 +8,6 @@ import java.util.function.Supplier;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
-import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.ctre.phoenix6.sim.ChassisReference;
@@ -93,7 +92,7 @@ public class ShooterSubsystem extends SubsystemBase {
         });
     }
 
-    
+
     @Override
     public void simulationPeriodic() {
         var talonFXSim = shooterMotorLeft.getSimState();
